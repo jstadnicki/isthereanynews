80da1f191808683736c6ff71dad24cfd4a6f2fb7
Jaroslaw Stadnicki <jstadnicki@corasystems.com>
2019-10-10 23:27:34 +0100

    groupying and counting news

--- Itan.Application/Class1.cs
+++ Itan.Application/Class1.cs
@@ -13,7 +13,7 @@ namespace Itan.Application
             var connectionString = "server=.\\sql2017;database=itan;User Id=itanreaduser;password=12qw!@QW";
             using (var connection = new SqlConnection(connectionString))
             {
-                var result = connection.Query<ChannelViewModel>("SELECT * FROM CHANNELS");
+                var result = connection.Query<ChannelViewModel>("select c.id,c.Url,count(n.Id) as NewsCount from Channels c left join News n on n.ChannelId = c.Id group by c.Id, c.Url");
                 return result.ToList();
             }
         }
