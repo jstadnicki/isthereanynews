b7ba51474829b12cb747a1ca879fd76978e44615
Jaroslaw Stadnicki <jstadnicki@corasystems.com>
2019-10-10 22:56:00 +0100

    simple dummy project that displays channels

---
+++ Itan.Api/Controllers/ValuesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Itan.Application;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Itan.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChannelsController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<ChannelViewModel>> Get()
+        {
+            var x = new ChannelsProvider();
+            return x.GetAll();
+        }
+    }
+
+
+}
---
+++ Itan.Application/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using Dapper;
+
+namespace Itan.Application
+{
+    public class ChannelsProvider
+    {
+        public List<ChannelViewModel> GetAll()
+        {
+            var connectionString = "server=.\\sql2017;database=itan;User Id=itanreaduser;password=12qw!@QW";
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var result = connection.Query<ChannelViewModel>("SELECT * FROM CHANNELS");
+                return result.ToList();
+            }
+        }
+    }
+
+    public class ChannelViewModel
+    {
+        public string Title { get; set; }
+        public string Url { get; set; }
+        public Guid Id { get; set; }
+        public int NewsCount { get; set; }
+    }
+}
