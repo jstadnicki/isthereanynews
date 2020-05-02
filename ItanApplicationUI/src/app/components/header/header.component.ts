488b39694bcc0819336194777066d160ab398b45
Jaroslaw Stadnicki <jaroslaw.stadnicki@gmail.com>
2020-05-02 14:47:37 +0200

    header cleanup

--- ItanApplicationUI/src/app/components/header/header.component.ts
+++ ItanApplicationUI/src/app/components/header/header.component.ts
@@ -1,76 +1,63 @@
-import { Component, OnInit } from "@angular/core";
-import { Logger, CryptoUtils } from "msal";
-import { HttpClient } from "@angular/common/http";
-import { Client } from "@microsoft/microsoft-graph-client";
+import {Component, OnInit} from "@angular/core";
+import {Logger, CryptoUtils} from "msal";
+import {HttpClient} from "@angular/common/http";
+import {Client} from "@microsoft/microsoft-graph-client";
 
-import { BroadcastService, MsalService } from "@azure/msal-angular";
+import {BroadcastService, MsalService} from "@azure/msal-angular";
 
 @Component({
-    selector: "app-header",
-    templateUrl: "./header.component.html",
-    styleUrls: ["./header.component.less"],
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.less"],
 })
 export class HeaderComponent implements OnInit {
-    profile;
-    account;
-    authenticated: any;
+  profile;
+  account;
+  authenticated: any;
 
-    constructor(
-        private broadcastService: BroadcastService,
-        private authService: MsalService,
-        private http: HttpClient
-    ) {
-    }
-    loggedIn = false;
+  constructor(
+    private broadcastService: BroadcastService,
+    private authService: MsalService,
+    private http: HttpClient
+  ) {
+  }
 
-    ngOnInit(): void {
-        this.broadcastService.subscribe("msal:loginSuccess", (e) => {
-            console.log("msal:loginSuccess");
-            this.checkAccount();
-        });
+  loggedIn = false;
 
-        this.authService.handleRedirectCallback((authError, response) => {
-            if (authError) {
-                console.error("Redirect Error: ", authError.errorMessage);
-                return;
-            }
+  ngOnInit(): void {
+    this.broadcastService.subscribe("msal:loginSuccess", (e) => {
+      this.checkAccount();
+    });
 
-            console.log("Redirect Success: ", response.accessToken);
-        });
-    }
+    this.authService.handleRedirectCallback((authError, response) => {
+      if (authError) {
+        return;
+      }
+    });
+  }
 
-    logout() {
+  logout() {
     this.authService.logout();
-    }
+  }
 
-    login() {
-        const accessTokenRequest = {
-             scopes: [
-                 'https://graph.microsoft.com/User.Read',
-                "https://isthereanynewscodeblast.onmicrosoft.com/05cd7635-e6f4-47c9-a5ce-8ec04368b297/application_reader",
-                "https://isthereanynewscodeblast.onmicrosoft.com/05cd7635-e6f4-47c9-a5ce-8ec04368b297/application_writer",
-             ],
-            clientId: "f1ab593c-f0b4-44da-85dc-d89a457745a9",
-            authority: "https://isthereanynewscodeblast.b2clogin.com/isthereanynewscodeblast.onmicrosoft.com/B2C_1_itansignup",
-            redirectUri: "http://localhost:4200",
-            postLogoutRedirectUri: 'http://localhost:4200/',
-        };
-        this.authService
-            .loginPopup(accessTokenRequest)
-            .then((res) => {
-                console.log("loginPopup");
-                console.log(res);
-            })
-            .catch((e) => {
-                console.log("acquireTokenPopup catch");
-                console.log(e);
-            });
-    }
+  login() {
+    const accessTokenRequest = {
+      scopes: [
+        'https://graph.microsoft.com/User.Read',
+        "https://isthereanynewscodeblast.onmicrosoft.com/05cd7635-e6f4-47c9-a5ce-8ec04368b297/application_reader",
+        "https://isthereanynewscodeblast.onmicrosoft.com/05cd7635-e6f4-47c9-a5ce-8ec04368b297/application_writer",
+      ],
+      clientId: "f1ab593c-f0b4-44da-85dc-d89a457745a9",
+      authority: "https://isthereanynewscodeblast.b2clogin.com/isthereanynewscodeblast.onmicrosoft.com/B2C_1_itansignup",
+      redirectUri: "http://localhost:4200",
+      postLogoutRedirectUri: 'http://localhost:4200/',
+    };
+    this.authService
+      .loginPopup(accessTokenRequest);
+  }
 
-    checkAccount() {
-        console.log("checkAccount");
-        this.account = this.authService.getAccount();
-        console.log(this.account);
-        this.loggedIn = !!this.account;
-    }
+  checkAccount() {
+    this.account = this.authService.getAccount();
+    this.loggedIn = !!this.account;
+  }
 }
