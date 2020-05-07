032db7a01d2afc5566508df4477124a63e6619ad
Jaroslaw Stadnicki <jaroslaw.stadnicki@gmail.com>
2020-05-07 06:53:18 +0200

    html strip angular

---
+++ ItanApplicationUI/src/app/components/channels-page/strip-html.pipe.ts
@@ -0,0 +1,10 @@
+﻿import {Pipe, PipeTransform} from "@angular/core";
+
+@Pipe({
+    name: 'striphtml'
+})
+export class StripHtmlPipe implements PipeTransform {
+    transform(value: string): any {
+        return value.replace(/<.*?>/g, ''); // replace tags
+    }
+}
