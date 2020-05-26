a4329d4a0eda3646ee5d3ec302fee08ce152e6e6
Jaroslaw Stadnicki <jaroslaw.stadnicki@gmail.com>
2020-05-26 11:47:28 +0200

    on login try to create person account if user doesnt have one already

--- Itan.Application.UI/src/app/components/header/header.component.ts
+++ Itan.Application.UI/src/app/components/header/header.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from "@angular/core";
 import {Logger, CryptoUtils} from "msal";
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {Client} from "@microsoft/microsoft-graph-client";
 
 import {BroadcastService, MsalService} from "@azure/msal-angular";
@@ -23,9 +23,18 @@ export class HeaderComponent implements OnInit {
   }
 
   loggedIn = false;
+  sessionId = this.createUUID();
+
+  private createUUID(): string {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+      var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+    });
+  }
 
   ngOnInit(): void {
     this.broadcastService.subscribe("msal:loginSuccess", (e) => {
+      this.createPersonAccount();
       this.checkAccount();
     });
 
@@ -58,11 +67,41 @@ export class HeaderComponent implements OnInit {
       .loginPopup(accessTokenRequest);
   }
 
-  private createUUID(): string {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-      var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-      return v.toString(16);
-    });
+  async createPersonAccount() {
+    const a = this.authService.getAccount();
+
+    const accessTokenRequest = {
+      scopes: [
+        "https://isthereanynewscodeblast.onmicrosoft.com/api/application_writer",
+      ],
+      clientId: "01805485-e711-4975-bbed-d10eb448d097",
+      authority:
+        "https://isthereanynewscodeblast.b2clogin.com/isthereanynewscodeblast.onmicrosoft.com/B2C_1_itansignup",
+      redirectUri: "http://localhost:4200",
+      account: a,
+      sid: this.sessionId
+    };
+
+    const x = await this.authService.acquireTokenSilent(accessTokenRequest);
+
+    const o = this.getOptions(x.accessToken);
+
+    let body = {
+      userId: a.accountIdentifier
+    }
+
+    this.http
+      .post("https://localhost:5001/api/users", body, o)
+      .subscribe(e => {
+        console.log(e);
+      });
+
+  }
+
+  private getOptions(token: string) {
+    return {
+      headers: new HttpHeaders({Authorization: `Bearer ${token}`}),
+    };
   }
 
   checkAccount() {
