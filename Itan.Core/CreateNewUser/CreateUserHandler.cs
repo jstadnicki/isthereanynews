32943a202fa7e1af8d1f9c653d2778ecef7a25ab
Jaroslaw Stadnicki <jaroslaw.stadnicki@gmail.com>
2020-05-26 11:47:53 +0200

    refactoring things

--- Itan.Core/CreateNewUser/CreateUserHandler.cs
+++ Itan.Core/CreateNewUser/CreateUserHandler.cs
@@ -6,28 +6,30 @@ using MediatR;
 
 namespace Itan.Core.CreateNewUser
 {
-    public class CreateUserHandler : AsyncRequestHandler<CreateNewUserRequest>
+    public class CreateUserHandler : IRequestHandler<CreateNewUserRequest>
     {
         private readonly ICreateUserRepository repository;
-
+        
         public CreateUserHandler(ICreateUserRepository repository)
         {
             this.repository = repository;
         }
-
-
-        protected override Task Handle(CreateNewUserRequest request, CancellationToken cancellationToken)
-        {
-            this.Validate(request);
-            return this.repository.CreateNewPersonAsync(request.UserId);
-        }
-
+        
+        
+        
         private void Validate(CreateNewUserRequest request)
         {
             if (Guid.Empty == request.UserId)
             {
                 throw new BadArgumentInRequestException(nameof(CreateUserHandler), nameof(request.UserId));
             }
+        }
+
+        public async Task<Unit> Handle(CreateNewUserRequest request, CancellationToken cancellationToken)
+        {
+            this.Validate(request);
+            await this.repository.CreateNewPersonAsync(request.UserId);
+            return await Task.FromResult(new Unit());
         }
     }
 }
