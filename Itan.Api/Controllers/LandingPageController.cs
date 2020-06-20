ee92aeda0e8a62bb90c7bef0ea327516dacc5a7e
Jaroslaw Stadnicki <jaroslaw.stadnicki@gmail.com>
2020-06-20 13:09:06 +0200

    landing page controller

---
+++ Itan.Api/Controllers/LandingPageController.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using Itan.Core;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace Itan.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [AllowAnonymous]
+    public class LandingPageController : ControllerBase
+    {
+        private readonly IConfiguration configuration;
+
+        public LandingPageController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+        
+        [Route("news")]
+        [HttpGet]
+        public async Task<ActionResult<HomePageNews>> Get()
+        {
+            var np = new NewsProvider(configuration);
+            var hpn = await np.GetHomePageNews();
+            return this.Ok(hpn);
+        }
+    }
+}
