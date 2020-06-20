7946782e7b8948b4fe3685da38ffb95c739177fe
Jaroslaw Stadnicki <jaroslaw.stadnicki@gmail.com>
2020-06-20 13:10:59 +0200

    landing page, references

---
+++ Itan.Core/HomePageNews.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Itan.Core
+{
+    public class HomePageNews
+    {
+        public List<LandingPageNews> TopNews { get; set; }
+        public List<LandingPageNews> BottomNews { get; set; }
+    }
+}
---
+++ Itan.Core/LandingPageNews.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Itan.Core
+{
+    public class LandingPageNews
+    {
+        public string Author { get; set; }
+        public Guid ChannelId { get; set; }
+        public string Title { get; set; }
+        public Guid Id { get; set; }
+        public DateTime Published { get; set; }
+        public string Link { get; set; }
+        public string Image { get; set; }
+        public string Content { get; set; }
+        public string ContentLink { get; set; }
+    }
+}
