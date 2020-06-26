90d7bd8ec4a4cf10eacc3222474940129e07d8d2
Jaroslaw Stadnicki <jaroslaw.stadnicki@gmail.com>
2020-06-27 01:22:51 +0200

    migration tool

---
+++ Itan.Database/Migrations/20200622204232_NewsAddSha.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Itan.Database.Migrations
+{
+    public partial class NewsAddSha : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "SHA256",
+                table: "News",
+                maxLength: 64,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SHA256",
+                table: "News");
+        }
+    }
+}
