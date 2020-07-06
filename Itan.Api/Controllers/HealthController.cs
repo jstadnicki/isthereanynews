fb4528cb484502d592c7df507e56b71371771ba0
Jaroslaw Stadnicki <jaroslaw.stadnicki@gmail.com>
2020-07-06 17:07:19 +0200

    simple healts controller

---
+++ Itan.Api/Controllers/HealthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Itan.Api.Controllers
+{
+    [AllowAnonymous]
+    [ApiController]
+    [Route("[controller]")]
+    public class HealthController : ControllerBase
+    {
+        public ActionResult<string> Ping()
+        {
+            return this.Ok("Pong");
+        }
+    }
+}
