90c9fbb2790dc8c483ce923b6192457ac93ae30a
Jaroslaw Stadnicki <jaroslaw.stadnicki@gmail.com>
2020-08-30 02:00:05 +0200

    AKV as settings provider

--- Itan.Api/Program.cs
+++ Itan.Api/Program.cs
@@ -1,7 +1,10 @@
 ﻿using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureKeyVault;
 
 namespace Itan.Api
 {
@@ -13,10 +16,21 @@ namespace Itan.Api
                 .Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            var astp = new AzureServiceTokenProvider();
+            var kvc = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(astp.KeyVaultTokenCallback));
+
+            return WebHost.CreateDefaultBuilder(args)
                 .ConfigureServices(x => x.AddAutofac())
-                .ConfigureAppConfiguration(x => { x.AddJsonFile("appsettings.json"); })
+                .ConfigureAppConfiguration((x, config) =>
+                {
+                    config.AddJsonFile("appsettings.json");
+                    config.AddAzureKeyVault("https://itan-kv-secrets.vault.azure.net", 
+                        kvc,
+                        new DefaultKeyVaultSecretManager());
+                })
                 .UseStartup<Startup>();
+        }
     }
 }
