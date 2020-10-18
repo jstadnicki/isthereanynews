b840ddb8f3c0e9718a9e05b0a6e6f3e0940d846c
Jaroslaw Stadnicki <jaroslaw.stadnicki@gmail.com>
2020-10-18 09:21:26 +0200

    itan function app instead of app service

--- terraform/itan-azure-rm-app-service-function.tf
+++ terraform/itan-azure-rm-app-service-function.tf
@@ -1,4 +1,4 @@
-﻿resource "azurerm_app_service" "itan-app-service-function" {
+﻿resource "azurerm_function_app" "itan-app-service-function" {
   app_service_plan_id = azurerm_app_service_plan.itan-app-service-plan-function.id
   location = var.location
   name = "itan-app-service-function"
@@ -9,4 +9,7 @@
   }
   
   depends_on = [azurerm_app_service_plan.itan-app-service-plan-function]
+  
+  storage_account_name = azurerm_storage_account.itan_storage_account.name
+  storage_account_access_key = azurerm_storage_account.itan_storage_account.primary_access_key
 }
