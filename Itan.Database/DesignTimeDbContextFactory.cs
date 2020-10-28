342990497109a2bc1ee44c73826232df85857ec4
Jaroslaw Stadnicki <jaroslaw.stadnicki@gmail.com>
2020-10-28 22:11:37 +0100

    database is being created based on AKV

--- Itan.Database/DesignTimeDbContextFactory.cs
+++ Itan.Database/DesignTimeDbContextFactory.cs
@@ -1,7 +1,10 @@
 ﻿using System.IO;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureKeyVault;
 
 namespace Itan.Database
 {
@@ -9,12 +12,21 @@ namespace Itan.Database
     {
         public EntityFrameworkContext CreateDbContext(string[] args)
         {
+            var astp = new AzureServiceTokenProvider();
+            var kvc = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(astp.KeyVaultTokenCallback));
+
             IConfigurationRoot configuration = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
                 .AddJsonFile("appsettings.json")
+                .AddAzureKeyVault("https://itan-key-vault.vault.azure.net", kvc, new DefaultKeyVaultSecretManager())
                 .Build();
+
             var builder = new DbContextOptionsBuilder<EntityFrameworkContext>();
-            var connectionString = configuration.GetConnectionString("itansql");
+            var connectionString =
+                kvc.GetSecretAsync("https://itan-key-vault.vault.azure.net/", "SqlAdminConnectionString")
+                   .GetAwaiter()
+                   .GetResult()
+                   .Value;
             builder.UseSqlServer(connectionString);
             return new EntityFrameworkContext(builder.Options);
         }
