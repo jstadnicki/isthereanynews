eea5fdd2e2505a29b8f2eb609b81aa65f6da60a9
Jaroslaw Stadnicki <jaroslaw.stadnicki@gmail.com>
2020-11-02 11:10:30 +0100

    storage account with static page host enabled

--- terraform/itan-azure-rm-storage-account.tf
+++ terraform/itan-azure-rm-storage-account.tf
@@ -3,6 +3,12 @@ resource "azurerm_storage_account" "itan_storage_account" {
   account_tier = "standard"
   location = var.location
   name = "itanstorageaccount"
-  resource_group_name = var.resource-group-name  
+  resource_group_name = var.resource-group-name
+
+  static_website {
+    index_document = "index.html"
+    error_404_document = "404.html"
+  }
+
   depends_on = [azurerm_resource_group.itan-west-europe-resource-group]
 }
