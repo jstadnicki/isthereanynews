f8dbb0d7b2bde9a057c951990dc627446ef6e72e
Jaroslaw Stadnicki <jaroslaw.stadnicki@gmail.com>
2020-11-15 10:54:08 +0100

    app insig ht

---
+++ terraform/terraform-azure-rm-application-insight.tf
@@ -0,0 +1,6 @@
+﻿resource "azurerm_application_insights" "itan-application-insights" {
+  application_type = "web"
+  location = var.location
+  name = "itan-application-insights"
+  resource_group_name = var.resource-group-name
+}
