73fdddbc80cd16f00e43b968eecb7b281aa52f88
Jaroslaw Stadnicki <jaroslaw.stadnicki@gmail.com>
2020-11-15 10:54:27 +0100

    AKV values fixed

--- terraform/itan-azure-rm-keyvault-secrets.tf
+++ terraform/itan-azure-rm-keyvault-secrets.tf
@@ -31,7 +31,7 @@ resource "azurerm_key_vault_secret" "itan-secret-tenant" {
 resource "azurerm_key_vault_secret" "itan-secret-storage" {
   name = "ConnectionStrings--Storage"
   key_vault_id = azurerm_key_vault.itan-key-vault.id
-  value = azurerm_storage_account.itan_storage_account.primary_access_key
+  value = azurerm_storage_account.itan_storage_account.primary_connection_string
   
   depends_on = [azurerm_storage_account.itan_storage_account]
 }
@@ -39,19 +39,19 @@ resource "azurerm_key_vault_secret" "itan-secret-storage" {
 resource "azurerm_key_vault_secret" "itan-secret-sql-reader" {
   name = "ConnectionStrings--SqlReader"
   key_vault_id = azurerm_key_vault.itan-key-vault.id
-  value = "server=tcp:${azurerm_mssql_server.itan-mssql-server.fully_qualified_domain_name};database=itan;User Id=itanreaduser;password=${random_password.itan-mssql-reader-password.result}"
+  value = "Server=tcp:${azurerm_mssql_server.itan-mssql-server.fully_qualified_domain_name},1433;Initial Catalog=itan;User Id=${var.itan-reader-mssql};Password=${random_password.itan-mssql-reader-password.result};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
 }
 
 resource "azurerm_key_vault_secret" "itan-secret-sql-writer" {
   name = "ConnectionStrings--SqlWriter"
   key_vault_id = azurerm_key_vault.itan-key-vault.id
-  value = "server=tcp:${azurerm_mssql_server.itan-mssql-server.fully_qualified_domain_name};database=itan;User Id=itanwriteuser;password=${random_password.itan-mssql-writer-password.result}"
+  value = "Server=tcp:${azurerm_mssql_server.itan-mssql-server.fully_qualified_domain_name},1433;Initial Catalog=itan;User Id=${var.itan-writer-mssql};Password=${random_password.itan-mssql-writer-password.result};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
 }
 
 resource "azurerm_key_vault_secret" "itan-secret-sql-connection" {
   name = "SqlAdminConnectionString"
   key_vault_id = azurerm_key_vault.itan-key-vault.id
-  value = "Server=tcp:${azurerm_mssql_server.itan-mssql-server.fully_qualified_domain_name} Database=${azurerm_mssql_database.itan-mssql-database.name};User ID=${var.itan-admin-mssql};Password=${random_password.itan-mssql-admin-password.result};Trusted_Connection=False;Encrypt=True;"
+  value = "Server=tcp:${azurerm_mssql_server.itan-mssql-server.fully_qualified_domain_name},1433 Database=${azurerm_mssql_database.itan-mssql-database.name};User ID=${var.itan-admin-mssql};Password=${random_password.itan-mssql-admin-password.result};Trusted_Connection=False;Encrypt=True;"
 }
 
 resource "azurerm_key_vault_secret" "itan-secret-admin-password" {
