94181b6c6b17c7a41ead87972a87d03d71850ee5
Jaroslaw Stadnicki <jaroslaw.stadnicki@gmail.com>
2020-11-18 16:19:16 +0100

    switch to AKV again

--- Itan.Database/DesignTimeDbContextFactory.cs
+++ Itan.Database/DesignTimeDbContextFactory.cs
@@ -1,11 +1,11 @@
 ﻿using System;
-using System.Collections;
 using System.IO;
 using Microsoft.Azure.KeyVault;
 using Microsoft.Azure.Services.AppAuthentication;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureKeyVault;
 
 namespace Itan.Database
 {
@@ -13,35 +13,24 @@ namespace Itan.Database
     {
         public EntityFrameworkContext CreateDbContext(string[] args)
         {
+            var astp = new AzureServiceTokenProvider();
+            var kvc = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(astp.KeyVaultTokenCallback));
 
             IConfigurationRoot configuration = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
                 .AddJsonFile("appsettings.json")
-                // .AddAzureKeyVault("https://itan-key-vault.vault.azure.net", kvc, new DefaultKeyVaultSecretManager())
+                .AddAzureKeyVault("https://itan-key-vault.vault.azure.net", kvc, new DefaultKeyVaultSecretManager())
                 .Build();
 
-            var connectionString = Environment.GetEnvironmentVariable("var-6");
-            var x1 = Environment.GetEnvironmentVariable("SqlAdminConnectionString", EnvironmentVariableTarget.Machine);
-            var x2 = Environment.GetEnvironmentVariable("SqlAdminConnectionString", EnvironmentVariableTarget.Process);
-            var x3 = Environment.GetEnvironmentVariable("SqlAdminConnectionString", EnvironmentVariableTarget.User);
-
-            var e = Environment.GetEnvironmentVariables();
-            foreach (DictionaryEntry dictionaryEntry in e)
-            {
-                Console.WriteLine($"K:{dictionaryEntry.Key} - V:{dictionaryEntry.Value}");
-            }
-
-            Console.WriteLine("CS:"+connectionString);
-            Console.WriteLine("x1:"+x1);
-            Console.WriteLine("x2:"+x2);
-            Console.WriteLine("x3:"+x3);
-
             var builder = new DbContextOptionsBuilder<EntityFrameworkContext>();
-            // var connectionString =
-            //     kvc.GetSecretAsync("https://itan-key-vault.vault.azure.net/", "SqlAdminConnectionString")
-            //        .GetAwaiter()
-            //        .GetResult()
-            //        .Value;
+            var connectionString = kvc
+                .GetSecretAsync("https://itan-key-vault.vault.azure.net/", "SqlAdminConnectionString")
+                .GetAwaiter()
+                .GetResult()
+                .Value;
+
+            Console.WriteLine(connectionString);
+
             builder.UseSqlServer(connectionString);
             return new EntityFrameworkContext(builder.Options);
         }
