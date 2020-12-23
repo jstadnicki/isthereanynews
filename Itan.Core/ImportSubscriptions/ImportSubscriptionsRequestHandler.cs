0fda07a5ed4169fcbff0cb90ae197071130b1f82
Jaroslaw Stadnicki <jaroslaw.stadnicki@gmail.com>
2020-12-23 11:14:36 +0100

    importing support case when channel already exists

--- Itan.Core/ImportSubscriptions/ImportSubscriptionsRequestHandler.cs
+++ Itan.Core/ImportSubscriptions/ImportSubscriptionsRequestHandler.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 using Itan.Core.Handlers;
 using MediatR;
@@ -9,23 +10,37 @@ namespace Itan.Core.ImportSubscriptions
     {
         private readonly ICreateNewChannelRepository createNewChannelRepository;
         private readonly IUserToChannelSubscriptionsRepository subscriptionsRepository;
+        private readonly IChannelFinderRepository channelFinderRepository;
 
 
         public ImportSubscriptionsRequestHandler(
             ICreateNewChannelRepository createNewChannelRepository,
-            IUserToChannelSubscriptionsRepository subscriptionsRepository)
+            IUserToChannelSubscriptionsRepository subscriptionsRepository,
+            IChannelFinderRepository channelFinderRepository)
         {
             this.createNewChannelRepository = createNewChannelRepository;
             this.subscriptionsRepository = subscriptionsRepository;
+            this.channelFinderRepository = channelFinderRepository;
         }
 
         public async Task<Unit> Handle(ImportSubscriptionsRequest request, CancellationToken cancellationToken)
         {
             foreach (var outline in request.Opml.Body.Outline)
             {
-                var channelId =  await this.createNewChannelRepository.SaveAsync(outline.XmlUrl, request.UserId);
+                if (string.IsNullOrWhiteSpace(outline.XmlUrl))
+                {
+                    continue;
+                }
+
+                var channelId = await this.channelFinderRepository.FindChannelIdByUrlAsync(outline.XmlUrl.ToLowerInvariant());
+                if (channelId == default(Guid))
+                {
+                    channelId = await this.createNewChannelRepository.SaveAsync(outline.XmlUrl.ToLowerInvariant(), request.UserId);
+                }
+
                 await this.subscriptionsRepository.CreateSubscriptionAsync(channelId, request.UserId);
             }
+
             return Unit.Value;
         }
     }
