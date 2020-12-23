18e9ab42a4515a076a0adecff2b7d72262f6b57a
Jaroslaw Stadnicki <jaroslaw.stadnicki@gmail.com>
2020-12-23 09:33:05 +0100

    import subscriptions request and handler

---
+++ Itan.Core/ImportSubscriptions/ImportSubscriptionsRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using MediatR;
+
+namespace Itan.Core.ImportSubscriptions
+{
+    public class ImportSubscriptionsRequest : IRequest
+    {
+        public Guid UserId { get; }
+        public Opml Opml { get; }
+
+        public ImportSubscriptionsRequest(Guid userId, Opml opml)
+        {
+            UserId = userId;
+            Opml = opml;
+        }
+    }
+}
---
+++ Itan.Core/ImportSubscriptions/ImportSubscriptionsRequestHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Itan.Core.Handlers;
+using MediatR;
+
+namespace Itan.Core.ImportSubscriptions
+{
+    public class ImportSubscriptionsRequestHandler : IRequestHandler<ImportSubscriptionsRequest, Unit>
+    {
+        private readonly ICreateNewChannelRepository createNewChannelRepository;
+        private readonly IUserToChannelSubscriptionsRepository subscriptionsRepository;
+
+
+        public ImportSubscriptionsRequestHandler(
+            ICreateNewChannelRepository createNewChannelRepository,
+            IUserToChannelSubscriptionsRepository subscriptionsRepository)
+        {
+            this.createNewChannelRepository = createNewChannelRepository;
+            this.subscriptionsRepository = subscriptionsRepository;
+        }
+
+        public async Task<Unit> Handle(ImportSubscriptionsRequest request, CancellationToken cancellationToken)
+        {
+            foreach (var outline in request.Opml.Body.Outline)
+            {
+                var channelId =  await this.createNewChannelRepository.SaveAsync(outline.XmlUrl, request.UserId);
+                await this.subscriptionsRepository.CreateSubscriptionAsync(channelId, request.UserId);
+            }
+            return Unit.Value;
+        }
+    }
+}
