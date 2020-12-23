ecd4c1d06b079aa9b38924461fe78be3fdca8d56
Jaroslaw Stadnicki <jaroslaw.stadnicki@gmail.com>
2020-12-23 09:34:09 +0100

    handling import of opml.xml

--- Itan.Api/Controllers/SubscriptionsController.cs
+++ Itan.Api/Controllers/SubscriptionsController.cs
@@ -1,8 +1,16 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
 using Itan.Core;
+using Itan.Core.ImportSubscriptions;
 using MediatR;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.WebUtilities;
 
 namespace Itan.Api.Controllers
 {
@@ -24,6 +32,23 @@ namespace Itan.Api.Controllers
             var command = new GetAllSubscribedChannelsViewModelsRequest {PersonId = userId};
             var list = await this.mediator.Send(command);
             return list;
+        }
+
+        [HttpPost]
+        [Route("import")]
+        public async Task<ActionResult> Post([FromForm(Name = "file")] IFormFile file)
+        {
+            var userId = Guid.Parse(this.User.Claims.Single(x => x.Type == "http://schemas.microsoft.com/identity/claims/objectidentifier").Value);
+            var stream = file.OpenReadStream();
+            var httpRequestStreamReader = new HttpRequestStreamReader(stream, Encoding.Default);
+            var xmlTextReader = new XmlTextReader(httpRequestStreamReader);
+            var xs = new XmlSerializer(typeof(Opml));
+            var opml = (Opml) xs.Deserialize(xmlTextReader);
+
+            var command = new ImportSubscriptionsRequest(userId, opml);
+
+            await this.mediator.Send(command);
+            return this.Ok();
         }
     }
 }
