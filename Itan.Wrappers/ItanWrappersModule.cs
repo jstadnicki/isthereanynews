90dd64eccb37682013c435085ec1ce9df8c766cc
Jaroslaw Stadnicki <jaroslaw.stadnicki@gmail.com>
2020-12-27 10:48:22 +0100

    #15 autofac module for wrappers project

---
+++ Itan.Wrappers/ItanWrappersModule.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using Autofac;
+using Module = Autofac.Module;
+
+namespace Itan.Wrappers
+{
+    public class ItanWrappersModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterAssemblyTypes(typeof(ISerializer).GetTypeInfo().Assembly).AsImplementedInterfaces(); //itan.wrappers
+            builder.RegisterGeneric(typeof(AzureQueueWrapper<>)).As(typeof(IQueue<>));
+        }
+    }
+}
