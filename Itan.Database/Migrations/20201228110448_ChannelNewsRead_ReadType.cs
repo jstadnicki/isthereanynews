1c6fdea982a98cd81a7b7616597de8a67afba3bc
Jaroslaw Stadnicki <jaroslaw.stadnicki@gmail.com>
2020-12-28 12:55:57 +0100

    #39 Update database so that read and skip read news are distinguished  - missed migration files

---
+++ Itan.Database/Migrations/20201228110448_ChannelNewsRead_ReadType.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Itan.Database.Migrations
+{
+    public partial class ChannelNewsRead_ReadType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ReadType",
+                table: "ChannelNewsReads",
+                nullable: false,
+                defaultValue: "Read");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ReadType",
+                table: "ChannelNewsReads");
+        }
+    }
+}
