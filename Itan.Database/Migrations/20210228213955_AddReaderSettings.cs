ec934141b7a5c2cbb282cf4be1b7b41ffb733cfc
Jaroslaw Stadnicki <jaroslaw.stadnicki@gmail.com>
2021-02-28 22:46:42 +0100

    #102, #103 I want to be able to disable option of reading of updated news, I want to be able to select an option to merge all current available updates into one-last one, so I dont have to scroll them all to the latest

---
+++ Itan.Database/Migrations/20210228213955_AddReaderSettings.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Itan.Database.Migrations
+{
+    public partial class AddReaderSettings : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ReaderSettings",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    PersonId = table.Column<Guid>(nullable: false),
+                    ShowUpdatedNews = table.Column<string>(maxLength: 25, nullable: true, defaultValue: "Show"),
+                    SquashNewsUpdates = table.Column<string>(maxLength: 25, nullable: true, defaultValue: "Show")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ReaderSettings", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ReaderSettings_Persons_PersonId",
+                        column: x => x.PersonId,
+                        principalTable: "Persons",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ReaderSettings_PersonId",
+                table: "ReaderSettings",
+                column: "PersonId",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ReaderSettings");
+        }
+    }
+}
