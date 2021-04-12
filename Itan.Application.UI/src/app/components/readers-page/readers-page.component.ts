6d5e12a876b91e9af7e28ef6caac0c1f6634bde8
Jaroslaw Stadnicki <jaroslaw.stadnicki@gmail.com>
2021-04-12 23:18:51 +0200

    #154 Reader can subscribe to selected channel on other reader subscription view

--- Itan.Application.UI/src/app/components/readers-page/readers-page.component.ts
+++ Itan.Application.UI/src/app/components/readers-page/readers-page.component.ts
@@ -3,6 +3,8 @@ import {MsalWrapperService} from "../../service/msal-wrapper.service";
 import {ReadersRepositoryService} from "./readers-repository.service";
 import {ReaderViewModel} from "../../../server/Itan/Core/GetAllReaders/ReaderViewModel";
 import {ReaderDetailsViewModel} from "../../../server/Itan/Core/GetReader/ReaderDetailsViewModel";
+import {ChannelsSubscriptionsServiceService} from "../../service/channels-subscriptions-service.service";
+import {ChannelViewModel} from "../../../server/Itan/Core/ChannelViewModel";
 
 @Component({
   selector: 'app-readers-page',
@@ -13,13 +15,17 @@ export class ReadersPageComponent implements OnInit {
   isLoggedIn: boolean;
   readers: ReaderViewModel[];
   selectedReader: ReaderViewModel;
-  selectedChannel:any;
+  selectedChannel: any;
   readersLoaded: boolean = false;
   selectedReaderDetails: ReaderDetailsViewModel;
+  notificationText: string="";
+  notificationSuccessful: boolean=false;
+  notificationTimeout: any;
 
   constructor(
     private msalWrapperService: MsalWrapperService,
-    private readersRepository: ReadersRepositoryService
+    private readersRepository: ReadersRepositoryService,
+    private subscriptionService: ChannelsSubscriptionsServiceService
   ) {
   }
 
@@ -34,6 +40,41 @@ export class ReadersPageComponent implements OnInit {
 
   async onReaderClick(reader: ReaderViewModel) {
     this.selectedReader = reader;
-    await this.readersRepository.GetReaderDetailsAsync(this.selectedReader.id,r => this.selectedReaderDetails = r);
+    await this.readersRepository.GetReaderDetailsAsync(this.selectedReader.id, r => this.selectedReaderDetails = r);
+  }
+
+  async onChannelSubscribeClick(id: any) {
+    this.subscriptionService.subscribeToChannel(id.toString())
+      .then(() => this.showSubscribeNotification(true))
+      .catch(() => this.showSubscribeNotification(false));
+  }
+  private showSubscribeNotification(successful: boolean) {
+    if (successful) {
+      this.notificationText = "subscription command executed successfully";
+    } else {
+      this.notificationText = "subscription command executed with error";
+    }
+    this.notificationSuccessful = successful;
+    this.setNotificationClearTimer();
+  }
+
+  private setNotificationClearTimer() {
+    if (this.notificationTimeout != null) {
+      clearTimeout(this.notificationTimeout);
+      this.notificationTimeout = null;
+    }
+    this.notificationTimeout = setTimeout(() => {
+        this.closeNotification();
+      }
+      , 3500);
+  }
+
+  closeNotification() {
+    if (this.notificationTimeout != null) {
+      clearTimeout(this.notificationTimeout);
+      this.notificationTimeout = null;
+    }
+    this.notificationText = "";
+    this.notificationTimeout = null;
   }
 }
